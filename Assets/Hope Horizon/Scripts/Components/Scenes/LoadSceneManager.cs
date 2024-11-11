d9c29c59208526d86aa80c8af69e3dea96ccf272
PurusThuc <phungthuc39@gmail.com>
2024-11-11 23:34:31 +0700

    + add UI for main scene

---
+++ Assets/Hope Horizon/Scripts/Components/Scenes/LoadSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Hope_Horizon.Scripts.Components
+{
+    public class LoadSceneManager : MonoBehaviour
+    {
+        public void ShowMainScene()
+        {
+            SceneManager.LoadScene("scene_main");
+        }
+
+        public void ShowGameScene()
+        {
+            SceneManager.LoadScene("scene_game");
+        }
+    }
+}
